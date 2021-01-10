e1c469256a9f9006a09ad9c2f071d11791061d69
Berkay Kuyubasi <berkaykuyubasi@hotmail.com>
2021-01-10 18:25:10 +0300

    .

--- chess/run.c
+++ chess/run.c
@@ -47,14 +47,6 @@ int playerHasValidMove()
 
 void run(void)
 {
-    black_rook1_moved = 0;
-    black_rook2_moved = 0;
-    white_rook1_moved = 0;
-    white_rook2_moved = 0;
-    white_king_moved = 0;
-    black_king_moved = 0;
-    player = 1;
-    number_of_pawns_temp = 32;
     char move[5], c;
 
     fflush(stdin);
